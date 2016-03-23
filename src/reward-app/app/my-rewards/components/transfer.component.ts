90b9654f393c03304c22d13db4a86488e724ed6b
nikrhes <rhesa.tanoto@accenture.com>
2016-03-23 11:20:50 +0700

    update on look n feel

--- src/reward-app/app/my-rewards/components/transfer.component.ts
+++ src/reward-app/app/my-rewards/components/transfer.component.ts
@@ -3,6 +3,7 @@ import { Router } from 'angular2/router';
 import {MatchMediaService} from '../../shared/services/match-media.service';
 import {LayoutService} from '../../shared/services/layout.service';
 import {ModalService} from '../../shared/services/modal.service';
+import {AccountService} from '../../shared/services/account.service';
 
 @Component({
     selector: 'transfer',
@@ -10,24 +11,28 @@ import {ModalService} from '../../shared/services/modal.service';
 })
 export class TransferComponent  {
     
-    starting = 1010;
     transferred;
     phone = '';
     
 	constructor (private _router: Router,
 		private _matchMediaService: MatchMediaService,
 		private _layoutService: LayoutService,
+        private _accountService: AccountService,
         private _modalService: ModalService) {
 		
 		this._layoutService.setCurrentPage('Transfer');
 		
 	}
     
+    getPointValue(){
+        return (this._accountService.selectedUserPhone.rewards) ? this._accountService.selectedUserPhone.rewards : 0;
+    }
+    
     getRemainingPoints(){
         if(this.transferred === "" || this.transferred === null || this.transferred === undefined){
-            return this.starting;
+            return  this.getPointValue();
         }else{
-            return (this.starting - this.transferred);
+            return ( this.getPointValue() - this.transferred);
         }
     }
 	
