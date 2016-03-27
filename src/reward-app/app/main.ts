34e0912da06d5371bcb175c0361e4cc6be59ffe5
Erikvdv <erikvandeven@Eriks-MacBook-Pro.local>
2016-03-27 20:10:08 +0700

    fixes for android and ios

--- src/reward-app/app/main.ts
+++ src/reward-app/app/main.ts
@@ -1,10 +1,12 @@
 ///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
-import {provide} from 'angular2/core';
+import {enableProdMode, provide} from 'angular2/core';
 import {HTTP_PROVIDERS, BaseRequestOptions, RequestOptions} from 'angular2/http';
 import {Headers} from "angular2/http";
 import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+
+
 
 class MyOptions extends BaseRequestOptions {
     
@@ -15,6 +17,7 @@ class MyOptions extends BaseRequestOptions {
 
 } 
 
+enableProdMode();
 bootstrap(AppComponent,[
 	HTTP_PROVIDERS, 
 	provide(RequestOptions, {useClass: MyOptions}),
