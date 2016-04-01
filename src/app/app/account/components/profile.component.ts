5bfb5fe9119fb5b9f51116717d50190efd5be5f2
thomassjy <tom.thomas.sj@gmail.com>
2016-04-01 13:14:50 +0700

    Add desktop view
    There's still some error in CSS

--- src/app/app/account/components/profile.component.ts
+++ src/app/app/account/components/profile.component.ts
@@ -4,10 +4,14 @@ import {MatchMediaService} from '../../shared/services/match-media.service';
 import {LayoutService} from '../../shared/services/layout.service';
 import {AccountService} from '../../shared/services/account.service';
 import {ModalService} from '../../shared/services/modal.service';
+import {SmartLeftMenuComponent} from '../../shared/components/smart-left-menu.component';
 
 @Component({
     selector: 'profile',
-    templateUrl: './app/account/components/profile.component.html'
+    templateUrl: './app/account/components/profile.component.html',
+    directives: [
+        SmartLeftMenuComponent
+    ]
 })
 export class ProfileComponent  {
     
